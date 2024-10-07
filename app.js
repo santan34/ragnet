a53662de284167576d2e2940fdd18288d670fd89
St <aaronndas@gmail.com>
2024-10-08 01:10:39 +0200

    user routes, user controller, auth middleware complete

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const userRoutes = require('./routes/userRoutes');
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+userRoutes(app);
 
 const port = 3000 || process.env.PORT;
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
